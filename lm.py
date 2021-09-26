13761740ad14bdbb81cebebae7a6ebb02a9f3c83
What <467045478@qq.com>
2021-09-27 00:42:46 +0800

    Add files via upload

--- lm.py
+++ lm.py
@@ -15,21 +15,6 @@ auth('17606619309','619309')
 api = "https://sctapi.ftqq.com/SCT42234TWKI7KpDbOsx9xCQrzLeBVkSx.send"
 
 
-def xuangu(context):
-    q = jq.query(
-            jq.valuation.code,
-            jq.valuation.market_cap
-        ).filter(
-            jq.valuation.market_cap.between(20,30)
-        ).order_by(
-    jq.valuation.market_cap.asc()
-    )
-    giao = jq.run_query(q)
-    print(giao)
-    buylist = list(giao['code'])
-    return buylist
-
-'''
-df=get_fundamentals(query(valuation))
-print(df[:4])
-'''
+#获取市值
+df=jq.get_fundamentals(query(valuation.code,valuation.market_cap).filter(valuation.market_cap.between(20,30)).order_by(valuation.market_cap.asc()))
+print(df[:10])
